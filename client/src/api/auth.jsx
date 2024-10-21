1c0b87d9755c1ffed723abef5ff7abc258394a5c
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-10-21 16:00:02 +0700

    feat: add currentUser and currentAdmin API calls with token authorization

---
+++ client/src/api/auth.jsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export const currentUser = async(token) =>
+  await axios.post(
+    "http://localhost:5000/api/current-user",
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+export const currentAdmin = async(token) => {
+  return await axios.post(
+    "http://localhost:5000/api/current-admin",
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+};
