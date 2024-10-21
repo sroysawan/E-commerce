575fa6532acf5fd4c442070533c708f98f05b90e
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-10-22 00:52:09 +0700

    feat(server): setting limit image = 20mb

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 // const categoryRoute = require('./routes/category')
 //middleware
 app.use(morgan('dev'))
-app.use(express.json())
+app.use(express.json({limit: '20mb'}))
 app.use(cors())
 // app.use('/api',authRouter)
 // app.use('/api',categoryRoute)
