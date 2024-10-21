18ae13f3f81427913aaf8c56070e151e2c8f7596
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-10-21 17:49:33 +0700

    feat: API call create, list, remove Category

--- client/src/api/Category.jsx
+++ client/src/api/Category.jsx
@@ -1,30 +1,26 @@
 import axios from "axios";
 
-export const createCategory = async(token,form) => {
-    //code body
-    return await axios.post('http://localhost:5000/api/category',
-        form,
-        { headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-    )
-}
-export const listCategory = async(token) => {
-    //code body
-    return await axios.get('http://localhost:5000/api/category',
-        { headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-    )
-}
-export const removeCategory = async(token,id) => {
-    //code body
-    return await axios.delete('http://localhost:5000/api/category/'+id,
-        { headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-    )
-}
+export const createCategory = async (token, form) => {
+  //code body
+  return await axios.post('http://localhost:5000/api/category', form, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+export const listCategory = async (token) => {
+  //code body
+  return await axios.get('http://localhost:5000/api/category', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+export const removeCategory = async (token, id) => {
+  //code body
+  return await axios.delete('http://localhost:5000/api/category/' + id, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
