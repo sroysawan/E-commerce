61acdff687dc9407bfc7fcb101906ac1319d50d2
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-10-22 22:57:02 +0700

    refactor(server): remove auth for list category

--- server/routes/category.js
+++ server/routes/category.js
@@ -6,7 +6,7 @@ const { create,list,remove } = require('../controllers/category')
 const { authCheck,adminCheck } = require('../middlewares/authCheck')
 // @ENDPOINT http://localhost:5000/api/category
 router.post('/category',authCheck,adminCheck,create)
-router.get('/category',authCheck,adminCheck,list)
+router.get('/category',list)
 router.delete('/category/:id',authCheck,adminCheck,remove)
 
 
