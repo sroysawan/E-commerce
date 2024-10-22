b5c3b18ba1b174ec09ff34e4fc199c7a49fea73f
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-10-22 23:01:04 +0700

    feat: create searchFilters on global state by zustand

--- client/src/store/ecom-store.jsx
+++ client/src/store/ecom-store.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { create } from 'zustand'
 import { persist,createJSONStorage } from 'zustand/middleware'
 import { listCategory } from '../api/category'
-import { listProduct } from '../api/product'
+import { listProduct,searchFilters } from '../api/product'
 
 const ecomStore = (set) => ({
     user: null,
@@ -34,7 +34,18 @@ const ecomStore = (set) => ({
             products: res.data
           })
         } catch (error) {}
-      }
+      },
+
+    actionSearchFilters : async (arg) => {
+        try {
+          const res = await searchFilters(arg)
+          set({
+            products: res.data
+          })
+        } catch (error) {}
+      },
+
+
 })
 
 const usePersist = {
