6674acbcc9cecc6086a709d655993e6344337e8f
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-10-25 17:50:09 +0700

    feat: add API call payment function with token-based authorization for creating Payment Intent

---
+++ client/src/api/stripe.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const payment = async(token) =>
+  await axios.post(
+    "http://localhost:5000/api/user/create-payment-intent",
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
