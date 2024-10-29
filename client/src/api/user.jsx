6a578addd1baae7b25e7426adc3a48a147752cc7
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-10-29 13:19:11 +0700

    feat: Add user cart and order API functions (listUserCart, saveAddress, saveOrder, historyUserCart) with token authorization

--- client/src/api/user.jsx
+++ client/src/api/user.jsx
@@ -8,3 +8,47 @@ export const createUserCart = async (token, cart) => {
       },
     });
   };
+
+
+export const listUserCart = async (token) => {
+    //code body
+    return await axios.get("http://localhost:5000/api/user/cart", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  };
+
+export const saveAddress = async (token,address) => {
+    //code body
+    return await axios.post("http://localhost:5000/api/user/address",
+        {
+            address
+        }, 
+        {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  };
+
+export const saveOrder = async (token,payload) => {
+    //code body
+    return await axios.post("http://localhost:5000/api/user/order",
+      payload, 
+        {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  };
+
+
+  export const historyUserCart = async (token) => {
+    //code body
+    return await axios.get("http://localhost:5000/api/user/order", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  };
