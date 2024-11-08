e997f810c74958ce604e8988716a064c8818e961
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-11-09 02:21:03 +0700

    feat: Add Swagger documentation endpoint at /docs

--- server/server.js
+++ server/server.js
@@ -4,25 +4,25 @@ const app = express()
 const morgan = require('morgan')
 const { readdirSync } = require('fs') //อ่าน dir เข้าไปอ่านในโฟลเดอร์ route แล้วimport อัตโนมัติ
 const cors = require('cors')
-// const authRouter = require('./routes/auth')
-// const categoryRoute = require('./routes/category')
+
+
+const { swaggerUi,swaggerSpec } = require('./config/swaggerConfig')
+
 //middleware
 app.use(morgan('dev'))
 app.use(express.json({limit: '20mb'}))
 app.use(cors())
-// app.use('/api',authRouter)
-// app.use('/api',categoryRoute)
+
+
+
+
+//swagger doc api
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+//step 3 router
 readdirSync('./routes').map((c)=> 
     app.use('/api',require('./routes/'+c))
 )
-
-//step 3 router
-// app.post('/api',(req,res)=> {
-//     //code
-//     const { username,password} = req.body
-//     console.log(username,password)
-//     res.send('Welcome TO NODEJS')
-// })
 
 //step 2 start server
 app.listen(5000,() => {
