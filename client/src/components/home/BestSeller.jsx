872586fc70be337e8f7e9c3492cf751b2ebaebee
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-11-10 00:21:45 +0700

    feat: Add BestSeller component to display top-selling products

---
+++ client/src/components/home/BestSeller.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import { listProductBy } from '../../api/product'
+import ProductCard from '../cart/ProductCard'
+import SwiperShowProduct from '../../utils/SwiperShowProduct'
+import { SwiperSlide } from 'swiper/react'
+const BestSeller = () => {
+
+    const [data, setData] = useState([])
+
+    useEffect(()=>{
+        loadData()
+    },[])
+    const loadData = () => {
+        listProductBy('sold','desc',7)
+        .then((res)=>{
+            // console.log(res.data)
+            setData(res.data)
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    }
+  return (
+    <SwiperShowProduct>
+      {
+        data?.map((item,index)=>
+          <SwiperSlide> 
+            <ProductCard key={index} item={item}/>
+          </SwiperSlide>
+        )
+      }
+    </SwiperShowProduct>
+  )
+}
+
+export default BestSeller
