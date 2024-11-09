9a62c75255eda383c7a751b2554679772c8dbcd3
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-11-10 00:28:48 +0700

    feat: Add Home component to display carousel, bestsellers, new products, and categories

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import ContentCarousel from '../components/home/ContentCarousel'
+import BestSeller from '../components/home/BestSeller'
+import NewProduct from '../components/home/NewProduct'
+import CategoryProduct from '../components/home/CategoryProduct'
 
 const Home = () => {
   return (
-    <div>
-      home
+    <div className='pt-8 container mx-auto '>
+      <ContentCarousel />
+      <p className='text-2xl text-center my-4'>สินค้าขายดี</p>
+      <BestSeller />
+      <p className='text-2xl text-center my-4'>สินค้าใหม่</p>
+      <NewProduct />
+      <CategoryProduct/>
     </div>
   )
 }
