d2e17b7858c7f3f9641c761a1bba39ff0e2fa47f
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-11-09 18:56:45 +0700

    feat: Create Manage page with TableUser component

--- client/src/pages/admin/Manage.jsx
+++ client/src/pages/admin/Manage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import TableUser from '../../components/admin/TableUser'
 
 const Manage = () => {
   return (
     <div>
-      Manage
+      <TableUser />
     </div>
   )
 }
