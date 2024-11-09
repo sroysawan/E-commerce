d92e4a9b1c86400439716b151b0b824c47b45e79
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-11-09 18:54:26 +0700

    feat: Create History page with HistoryCart component

---
+++ client/src/pages/user/History.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HistoryCart from '../../components/cart/HistoryCart'
+
+const History = () => {
+  return (
+    <div>
+      <HistoryCart />
+    </div>
+  )
+}
+
+export default History
