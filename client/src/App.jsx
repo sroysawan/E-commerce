9614e0444b0d44362d72f5b4c8d1162f6d4b3083
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-11-09 19:02:10 +0700

    fix: import App.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,17 @@
-import React from 'react'
-import AppRoutes from './routes/AppRoutes'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import React from "react";
+import AppRoutes from "./routes/AppRoutes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 const App = () => {
   //JavaScript
-
-
-
   return (
     //html
-  <>
-    <ToastContainer pauseOnFocusLoss={false} newestOnTop autoClose={2000}/>
-    <AppRoutes />
-  </>
-  )
-}
+    <>
+      <ToastContainer pauseOnFocusLoss={false} newestOnTop autoClose={2000} />
+      <AppRoutes />
+    </>
+  );
+};
 
-export default App
+export default App;
