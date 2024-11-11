6e150bb3b68c5e6bf2d598b1b727217e00f64ba4
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-11-11 21:11:24 +0700

    feat: add update category name functionality and add error handling in update endpoint

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -27,6 +27,27 @@ exports.list = async(req,res)=> {
         })
     }
 }
+
+exports.update = async(req,res)=>{
+    try {
+        const { id } = req.params
+        const { name} = req.body
+        const category = await prisma.category.update({
+            where: {
+                id: Number(id),
+            },
+            data: {
+                name: name
+            }
+        })
+        res.send(category)
+    } catch (error) {
+        res.status(500).json({
+            message: "Server Error"
+        })
+    }
+}
+
 exports.remove = async(req,res)=> {
     try {
         const { id } = req.params
