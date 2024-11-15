757b3c7cc5e6e8acd9ca596b08373f3a6b3436e9
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-11-15 20:49:54 +0700

    feat: add listByCategory and updateCategory functions for category API integration

--- client/src/api/Category.jsx
+++ client/src/api/Category.jsx
@@ -14,6 +14,21 @@ export const listCategory = async () => {
   return await axios.get('http://localhost:5000/api/category');
 };
 
+export const listByCategory = async (id) => {
+  //code body
+  return await axios.get("http://localhost:5000/api/category/" + id
+  );
+};
+
+export const updateCategory = async (token, id ,form) => {
+  //code body
+  return await axios.put("http://localhost:5000/api/category/" + id,form, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
 export const removeCategory = async (token, id) => {
   //code body
   return await axios.delete('http://localhost:5000/api/category/' + id, {
@@ -22,3 +37,4 @@ export const removeCategory = async (token, id) => {
     },
   });
 };
+
