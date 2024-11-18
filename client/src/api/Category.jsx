5de94178303d44a30eddbe1553522b8a9c0f7760
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-11-18 17:20:09 +0700

    feat: Add pagination support for category listing and refactor API methods

--- client/src/api/Category.jsx
+++ client/src/api/Category.jsx
@@ -2,27 +2,41 @@ import axios from "axios";
 
 export const createCategory = async (token, form) => {
   //code body
-  return await axios.post('http://localhost:5000/api/category', form, {
+  return await axios.post("http://localhost:5000/api/category", form, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
 };
 
-export const listCategory = async () => {
+// export const listCategory = async () => {
+//   return await axios.get('http://localhost:5000/api/category');
+// };
+
+export const listCategory = async (token,page = 1, limit = 10) => {
   //code body
-  return await axios.get('http://localhost:5000/api/category');
+  return await axios.get("http://localhost:5000/api/category", {
+    params: { page, limit },
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+
+// หากต้องการดึงข้อมูลทั้งหมด
+const getCategoryWithoutPagination = async (token) => {
+  return await listCategory(1, 0); // กำหนด limit เป็น 0 เพื่อดึงข้อมูลทั้งหมด
 };
 
 export const listByCategory = async (id) => {
   //code body
-  return await axios.get("http://localhost:5000/api/category/" + id
-  );
+  return await axios.get("http://localhost:5000/api/category/" + id);
 };
 
-export const updateCategory = async (token, id ,form) => {
+export const updateCategory = async (token, id, form) => {
   //code body
-  return await axios.put("http://localhost:5000/api/category/" + id,form, {
+  return await axios.put("http://localhost:5000/api/category/" + id, form, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
@@ -31,10 +45,9 @@ export const updateCategory = async (token, id ,form) => {
 
 export const removeCategory = async (token, id) => {
   //code body
-  return await axios.delete('http://localhost:5000/api/category/' + id, {
+  return await axios.delete("http://localhost:5000/api/category/" + id, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
 };
-
