4ae86e38346b907eaed3c37fac0968370a52030e
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-11-29 23:37:20 +0700

    feat: add image data banner

---
+++ client/src/utils/mockData.jsx
@@ -0,0 +1,26 @@
+export const mockImages = [
+    { url: "https://picsum.photos/id/0/400/300" },
+    { url: "https://picsum.photos/id/1/400/300" },
+    { url: "https://picsum.photos/id/2/400/300" },
+    { url: "https://picsum.photos/id/3/400/300" },
+    { url: "https://picsum.photos/id/4/400/300" },
+    { url: "https://picsum.photos/id/5/400/300" },
+    { url: "https://picsum.photos/id/6/400/300" },
+    { url: "https://picsum.photos/id/7/400/300" },
+    { url: "https://picsum.photos/id/8/400/300" },
+    { url: "https://picsum.photos/id/9/400/300" },
+  ];
+
+export const mockImagesBanner = [
+    { url: "https://picsum.photos/id/0/1920/1080" },
+    { url: "https://picsum.photos/id/1/1920/1080" },
+    { url: "https://picsum.photos/id/2/1920/1080" },
+    { url: "https://picsum.photos/id/3/1920/1080" },
+    { url: "https://picsum.photos/id/4/1920/1080" },
+    { url: "https://picsum.photos/id/5/1920/1080" },
+    { url: "https://picsum.photos/id/6/1920/1080" },
+    { url: "https://picsum.photos/id/7/1920/1080" },
+    { url: "https://picsum.photos/id/8/1920/1080" },
+    { url: "https://picsum.photos/id/9/1920/1080" },
+
+  ];
