bf689bbe28fc5ad19363672bded30854c4a0d27e
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-11-30 22:49:04 +0700

    feat: dupicate code

--- client/src/pages/admin/Category.jsx
+++ client/src/pages/admin/Category.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import FormCategory from "../../components/admin/FormCategory";
 import FormCategoryPagination from "../../components/admin/FormCaregoryPagination";
 
 const Category = () => {
   return (
     <div className="container mx-auto">
-      {/* <FormCategory /> */}
       <FormCategoryPagination/>
     </div>
   );
