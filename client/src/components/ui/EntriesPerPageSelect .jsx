e0672af22e9c7445885f592e44db279b415a9c81
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-08 22:22:21 +0700

    refactor: Improve EntriesPerPageSelect component for better styling and responsiveness

--- client/src/components/ui/EntriesPerPageSelect .jsx
+++ client/src/components/ui/EntriesPerPageSelect .jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
 const EntriesPerPageSelect = ({ limit, total, onLimitChange, totalItems }) => {
+  
   return (
     <div className="flex justify-between items-center">
-      <div className="flex items-center space-x-2">
+      <div className="flex items-center space-x-2 text-sm md:text-base">
         <label>Show</label>
         <select
           value={limit}
           onChange={onLimitChange}
-          className="border rounded-md py-0.5 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="border rounded-md py-0.5 px-2.5 md:px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
           <option value={5}>5</option>
           <option value={10}>10</option>
