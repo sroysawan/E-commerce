4598c990b93bd9f3e9d53d5f318aff937c41a80a
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-08 22:38:59 +0700

    eat: Enhance createSlug utility  function to support Thai language

--- client/src/utils/slugFormat.jsx
+++ client/src/utils/slugFormat.jsx
@@ -3,6 +3,14 @@
 //     return name.toLowerCase().replace(/\s+/g, "-"); // เปลี่ยนช่องว่างเป็น - และทำให้พิมพ์เล็กทั้งหมด
 //   }
 export const createSlug = (name) => {
+
+  if (!name) return '';
+  
+  // ตรวจสอบว่าเป็นข้อความภาษาไทยหรือไม่ โดยใช้ช่วงรหัส Unicode ของภาษาไทย
+  if (/[\u0E00-\u0E7F]/.test(name)) {
+    // ถ้าเป็นภาษาไทย ให้เข้ารหัส URL แบบพิเศษ
+    return encodeURIComponent(name.toLowerCase().trim());
+  }
   return name
     .toLowerCase() // แปลงเป็นตัวพิมพ์เล็ก
     .replace(/[^a-z0-9\s-]/g, "") // ลบอักขระพิเศษ เช่น (), /, ฯลฯ
