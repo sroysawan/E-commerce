a68583590083edd9d3794f4c69ce8a51befdba98
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-08 22:45:52 +0700

    feat: Add product range display to PaginationProduct component

--- client/src/components/ui/PaginationProduct.jsx
+++ client/src/components/ui/PaginationProduct.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Pagination } from "@mui/material";
-const PaginationProduct = ({ totalPages, currentPage, onPageChange }) => {
+const PaginationProduct = ({ totalPages, currentPage, onPageChange,totalProducts,startIndex,endIndex }) => {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center md:justify-between items-center">
+    <p className='hidden md:block text-xs md:text-base text-gray-400'>Showing {startIndex} - {endIndex} of {totalProducts} Products </p>
+
     <Pagination
       count={totalPages} // จำนวนหน้าทั้งหมด
       page={currentPage} // หน้าปัจจุบัน
       onChange={onPageChange} // ฟังก์ชันเมื่อเปลี่ยนหน้า
       color="error"
-
       showFirstButton
       showLastButton
     />
