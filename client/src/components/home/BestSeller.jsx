4d51320b7f90f1fb20d79d5b9b12999c8bb8a1ea
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-08 22:20:47 +0700

    feat: Update BestSeller component for improved product display and data handling

--- client/src/components/home/BestSeller.jsx
+++ client/src/components/home/BestSeller.jsx
@@ -1,36 +1,41 @@
-import React, { useEffect, useState } from "react"
-import { listProductBy } from "../../api/product"
-import ProductCard from "../cart/ProductCard"
-import SwiperShowProduct from "../../utils/SwiperShowProduct"
-import { SwiperSlide } from "swiper/react"
+import React, { useEffect, useState } from "react";
+import { listProductBy } from "../../api/product";
+import ProductCard from "../cart/ProductCard";
+import SwiperShowProduct from "../../utils/SwiperShowProduct";
+import { SwiperSlide } from "swiper/react";
 const BestSeller = () => {
+  const [data, setData] = useState([]);
 
-  const [data, setData] = useState([])
-
-  useEffect(()=>{
-    loadData()
-  },[])
+  useEffect(() => {
+    loadData();
+  }, []);
   const loadData = () => {
-    listProductBy("sold", "desc", 7)
+
+    listProductBy("sold", "desc", 10)
       .then((res) => {
-        // console.log(res.data)
-        setData(res.data)
+        setData(res.data);
+
       })
       .catch((error) => {
-        console.log(error)
-      })
-  }
-  return (
-    <SwiperShowProduct>
-      {
-        data?.map((item, index) => 
-          <SwiperSlide>
-            <ProductCard key={index} item={item} />
-          </SwiperSlide>
-        )
-      }
-    </SwiperShowProduct>
-  )
-}
+        console.log(error);
 
-export default BestSeller
+      });
+  };
+  return (
+    <>
+    {data && data.length > 0 ? ( // ตรวจสอบว่ามีข้อมูลใน data
+      <SwiperShowProduct>
+        {data.map((item) => (
+          <SwiperSlide key={item.id}>
+            <ProductCard item={item} />
+          </SwiperSlide>
+        ))}
+      </SwiperShowProduct>
+    ) : (
+      <p className="text-center text-gray-500">ไม่มีสินค้าแนะนำในขณะนี้</p>// ข้อความเมื่อไม่มีข้อมูลและ Skeleton ถูกซ่อน
+    )}
+  </>
+  );
+};
+
+export default BestSeller;
