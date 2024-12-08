77d06ec7c27b3b1cb5fe75906e1b5f23aebf0dd3
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-08 22:30:03 +0700

    feat: Add export index for Skeleton UI components

---
+++ client/src/components/ui/Skeletons/index.jsx
@@ -0,0 +1,3 @@
+export { default as SkeletonProductCard } from "./SkeletonProductCard";
+export { default as SkeletonSearchCard } from "./SkeletonSearchCard";
+export { default as FullPageSkeleton  } from "./FullPageSkeleton";
