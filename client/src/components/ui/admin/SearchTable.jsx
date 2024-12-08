6a364224b581bc2acc8f0f7e962282619fb1fb43
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-08 22:23:21 +0700

    refactor: Enhance SearchTable component with improved styling and dynamic placeholder text

--- client/src/components/ui/admin/SearchTable.jsx
+++ client/src/components/ui/admin/SearchTable.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const SearchTable = ({handleSearch}) => {
+const SearchTable = ({handleSearch,textSearch}) => {
   return (
-    <div className="flex items-center space-x-4">
+    <div className="flex items-center space-x-4 text-sm md:text-base">
           <p>ค้นหา</p>
           <input
             type="text"
             onChange={handleSearch}
-            placeholder="Search User"
-            className="border border-gray-300 rounded-lg px-4 py-2 focus:ring-1 focus:outline-none focus:ring-blue-500"
+            placeholder={textSearch}
+            className="border border-gray-300 rounded-lg px-2 md:px-4 py-1.5 md:py-2 focus:ring-1 focus:outline-none focus:ring-blue-500"
           />
         </div>
   )
