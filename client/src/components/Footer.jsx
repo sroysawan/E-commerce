12349c18447e088f5443969066d1964bd09e755e
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-11 21:16:57 +0700

    feat: Add Footer component with copyright information

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => (
+    <footer className="w-full bg-black text-gray-300 py-3 text-center text-sm">
+      © {new Date().getFullYear()} E-commerce. All rights reserved.
+    </footer>
+  );
+  
+  export default Footer;
+  
