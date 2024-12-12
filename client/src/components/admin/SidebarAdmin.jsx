f528f69122fb2d28029a7ecbac397a2ac1c54e91
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-12 15:30:24 +0700

    refactor: Update SidebarAdmin component to use xl breakpoint for layout adjustments

--- client/src/components/admin/SidebarAdmin.jsx
+++ client/src/components/admin/SidebarAdmin.jsx
@@ -1,18 +1,15 @@
 import React, { useState } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import {
   ChartBarStacked,
   LayoutDashboard,
   ListOrdered,
-  LogOut,
   Menu,
   ShoppingCart,
   UserRoundCog,
 } from "lucide-react";
-import useEcomStore from "../../store/ecom-store";
+
 const SidebarAdmin = () => {
-  const actionLogout = useEcomStore((state) => state.actionLogout);
-  const navigate = useNavigate();
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   
   const toggleSidebar = () => {
@@ -23,17 +20,17 @@ const SidebarAdmin = () => {
     <>
       {/* Hamburger Button */}
       <button
-        className="md:hidden fixed top-3 left-2 z-40 md:z-50 text-gray-600 hover:text-red-500 p-2"
+        className="xl:hidden fixed top-3 left-2 z-40 xl:z-50 text-gray-600 hover:text-red-500 p-2"
         onClick={toggleSidebar}
       >
         {isSidebarOpen ? "" : <Menu size={28}/>}
       </button>
 
       {/* Sidebar */}
-      <div className={`fixed md:relative inset-y-0 left-0 z-50 md:z-40  bg-gray-900 text-gray-100 w-52 md:w-72 transform
-        ${isSidebarOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"} md:translate-x-0 transition-transform duration-300 ease-in-out
+      <div className={`fixed xl:relative inset-y-0 left-0 z-50 xl:z-40  bg-gray-900 text-gray-100 w-52 xl:w-72 transform
+        ${isSidebarOpen ? "translate-x-0" : "-translate-x-full xl:translate-x-0"} xl:translate-x-0 transition-transform duration-300 ease-in-out
         `}>
-        <div className="h-24 bg-gray-900 flex flex-col items-center justify-center text-lg md:text-2xl font-bold">
+        <div className="h-24 bg-gray-900 flex flex-col items-center justify-center text-lg xl:text-2xl font-bold">
           <p>Admin Panel</p>
         </div>
 
@@ -96,7 +93,7 @@ const SidebarAdmin = () => {
        {/* Overlay for Sidebar in Mobile */}
        {isSidebarOpen && (
         <div
-          className="fixed inset-0 bg-black opacity-50 z-30 md:hidden"
+          className="fixed inset-0 bg-black opacity-50 z-30 xl:hidden"
           onClick={toggleSidebar}
         />
       )}
