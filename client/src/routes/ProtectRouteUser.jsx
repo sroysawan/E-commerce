7bca7c87bc9433db372e9b5c6da661a277da7d06
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-13 21:07:47 +0700

    refactor: Update ProtectRouteUser component to use PrivateRoute and handle null state

--- client/src/routes/ProtectRouteUser.jsx
+++ client/src/routes/ProtectRouteUser.jsx
@@ -2,20 +2,33 @@ import React, { useEffect, useState } from "react";
 import useEcomStore from "../store/ecom-store";
 import { currentUser } from "../api/auth";
 import LoadingToRedirect from "../routes/LoadingToRedirect";
+import PrivateRoute from "./PrivateRoute";
 const ProtectRouteUser = ({ element }) => {
-  const [ok, setOk] = useState(false);
+  // const [ok, setOk] = useState(false);
+  const [ok, setOk] = useState(null); // เริ่มต้นด้วย null เพื่อไม่แสดงผลอะไร
   const user = useEcomStore((state) => state.user);
   const token = useEcomStore((state) => state.token);
 
+  // useEffect(() => {
+  //   if (user && token) {
+  //     // send to back
+  //     currentUser(token)
+  //       .then((res) => setOk(true))
+  //       .catch((error) => setOk(false));
+  //   }
+  // }, []);
+  // return ok ? element : <LoadingToRedirect />;
   useEffect(() => {
     if (user && token) {
-      // send to back
       currentUser(token)
-        .then((res) => setOk(true))
-        .catch((error) => setOk(false));
+        .then(() => setOk(true))
+        .catch(() => setOk(false));
+    } else {
+      setOk(false); // ถ้าไม่มี token หรือ user ก็ Redirect ทันที
     }
-  }, []);
-  return ok ? element : <LoadingToRedirect />;
+  }, [user, token]);
+
+  return <PrivateRoute ok={ok}>{element}</PrivateRoute>;
 };
 
 export default ProtectRouteUser;
