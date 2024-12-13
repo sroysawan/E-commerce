2c7fa277fa3e43e2d3c475fc4481330199d8e3c4
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-13 20:54:56 +0700

    feat: Add CartCheck component for displaying the cart list

---
+++ client/src/pages/CartCheck.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ListCart from "../components/cart/ListCart";
+
+const CartCheck = () => {
+  return (
+    <div className="container mx-auto my-4 xl:px-0 xl:mt-8 xl:mb-0">
+       <ListCart />
+    </div>
+  );
+};
+
+export default CartCheck;
