a973d920ef2c05158419ba9f944afeacf551c5bf
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-13 20:24:28 +0700

    refactor: Adjust SearchTable for consistent styling on larger screens

--- client/src/components/ui/admin/SearchTable.jsx
+++ client/src/components/ui/admin/SearchTable.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const SearchTable = ({handleSearch,textSearch}) => {
   return (
-    <div className="flex items-center space-x-4 text-sm md:text-base">
+    <div className="flex items-center space-x-4 text-sm xl:text-base">
           <p>ค้นหา</p>
           <input
             type="text"
             onChange={handleSearch}
             placeholder={textSearch}
-            className="border border-gray-300 rounded-lg px-2 md:px-4 py-1.5 md:py-2 focus:ring-1 focus:outline-none focus:ring-blue-500"
+            className="border border-gray-300 rounded-lg px-2 xl:px-4 py-1.5 xl:py-2 focus:ring-1 focus:outline-none focus:ring-blue-500"
           />
         </div>
   )
