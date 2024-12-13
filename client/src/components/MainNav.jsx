de6cfae5941f8beeb3cec42d44183507d0ebed5b
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-13 21:58:49 +0700

    feat: Refactor MainNav component for improved search and profile toggle functionality responsive

--- client/src/components/MainNav.jsx
+++ client/src/components/MainNav.jsx
@@ -13,54 +13,51 @@ import {
 import Icon from "../assets/icon.png";
 import Badge from "@mui/material/Badge";
 import SearchText from "./ui/SearchText";
+import { Search } from "lucide-react";
 const MainNav = () => {
   const carts = useEcomStore((state) => state.carts);
   const user = useEcomStore((state) => state.user);
   const actionLogout = useEcomStore((state) => state.actionLogout);
   const navigate = useNavigate();
 
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
-  const dropdownRef = useRef(null);
 
-  // Function to handle outside click
-  const handleClickOutside = (event) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-      setIsOpen(false);
-    }
+  const toggleProfile = () => {
+    setIsProfileOpen(!isProfileOpen);
   };
 
-  // useEffect to add and clean up the event listener
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
+  const toggleSearch = () => {
+    setIsSearchOpen(!isSearchOpen);
+  };
 
   const logoutRedirect = () => {
     actionLogout();
     navigate("/login");
-    setIsMenuOpen(false);
+    setIsProfileOpen(false);
   };
 
-    // Define the handleSearch function
-    const handleSearch = (query) => {
-      if (query.trim() !== "") {
-        navigate(`/product?query=${encodeURIComponent(query)}`);
-      }
-    };
+  // Define the handleSearch function
+  // const handleSearch = (query) => {
+  //   if (query.trim() !== "") {
+  //     navigate(`/product?query=${encodeURIComponent(query)}`);
+  //     setIsSearchOpen(false); // Close popup after search
+  //   }
+  // };
+  
 
   return (
-    <header className="bg-white shadow-lg sticky top-0 z-50">
-      <div className="mx-2 md:container md:mx-auto flex h-20 items-center gap-8">
+    <header className="bg-white shadow-lg sticky top-0 z-30">
+      <div className="container mx-auto px-2 sm:px-2 md:px-0 lg:px-2 xl:px-0 flex h-20 items-center gap-8">
         <Link
           to={"/"}
           className="text-red-500 flex items-center justify-center gap-3"
         >
-          <img src={Icon} className="w-10" alt="Logo" />
-          <p className="font-black text-xl py-1">E-commerce</p>
+          <img src={Icon} className="w-8 md:w-10" alt="Logo" />
+          <p className="font-black text-lg xl:text-xl py-1">E-commerce</p>
         </Link>
+
         <div className="flex flex-1 items-center justify-end md:justify-between">
           {/* Desktop Menu */}
           <nav className="hidden md:flex md:flex-1 md:justify-between">
@@ -91,18 +88,24 @@ const MainNav = () => {
                 </NavLink>
               </li>
             </ul>
-            <div className="flex items-center">
-              <SearchText onSearch={handleSearch}/>
+            <div className="hidden lg:flex items-center">
+              <SearchText 
+              setIsSearchOpen={setIsSearchOpen}
+              />
             </div>
             <div className="flex items-center gap-4">
+              <button
+                onClick={toggleSearch}
+                className="lg:hidden w-10 h-10 flex items-center justify-center rounded-full bg-gray-300 hover:text-red-500"
+              >
+                <Search size={20} />
+              </button>
               <div className="relative flex items-center justify-center">
                 <NavLink
                   to={"/cart"}
                   className={({ isActive }) =>
                     `w-10 h-10 flex items-center justify-center rounded-full bg-gray-300 transition ${
-                      isActive
-                        ? "text-red-500"
-                        : "hover:text-red-500"
+                      isActive ? "text-red-500" : "hover:text-red-500"
                     }`
                   }
                 >
@@ -115,57 +118,62 @@ const MainNav = () => {
                 </NavLink>
               </div>
 
+              {/* Profile  */}
               <div className="sm:flex sm:gap-4">
                 {user ? (
-                  <div className="relative" ref={dropdownRef}>
+                  <div className="relative">
                     <div className="flex items-center gap-2">
-                      <button
-                        type="button"
-                        onClick={() => setIsOpen(!isOpen)}
-                        className=""
-                      >
+                      <button type="button" onClick={toggleProfile}>
                         <div className="flex items-center gap-2">
                           <div className="w-10 h-10 flex items-center justify-center rounded-full bg-gray-300 hover:border-red-500">
                             <User />
                           </div>
                           <span className=" text-red-500 transition hover:text-red-700">
-                            {user.name.substring(0, user.name.indexOf(' ')) || user.name}
+                            {user.name.substring(0, user.name.indexOf(" ")) ||
+                              user.name}
                           </span>
                         </div>
                       </button>
                     </div>
-                    {isOpen && (
-                      <div className="absolute top-10 right-0 z-10 mt-2 w-56 divide-y divide-gray-100 rounded-md border border-gray-100 bg-white shadow-xl">
-                        {user.role === "admin" && (
+
+                    {isProfileOpen && (
+                      <>
+                        <div
+                          className="fixed inset-0 bg-transparent z-10 hidden md:block"
+                          onClick={() => setIsProfileOpen(false)}
+                        />
+                        <div className="absolute top-10 right-0 z-20 mt-2 w-56 divide-y divide-gray-100 rounded-md border border-gray-100 bg-white shadow-xl">
+                          {user.role === "admin" && (
+                            <div className="p-2">
+                              <NavLink
+                                to={"/admin"}
+                                className="p-2 flex items-center gap-2 rounded-lg  text-sm text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                              >
+                                <UserRoundPen />
+                                Dashboard Admin
+                              </NavLink>
+                            </div>
+                          )}
                           <div className="p-2">
                             <NavLink
-                              to={"/admin"}
+                              to={"/user/history"}
                               className="p-2 flex items-center gap-2 rounded-lg  text-sm text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                             >
-                              <UserRoundPen />
-                              Dashboard Admin
+                              <History />
+                              History
                             </NavLink>
                           </div>
-                        )}
-                        <div className="p-2">
-                          <NavLink
-                            to={"/user/history"}
-                            className="p-2 flex items-center gap-2 rounded-lg  text-sm text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                          >
-                            <History />
-                            History
-                          </NavLink>
+                          <div className="p-2 flex items-center gap-2">
+                            <button
+                              onClick={logoutRedirect}
+                              className="w-full p-2 flex items-center gap-2  rounded-lg  text-sm text-red-700 hover:bg-red-100"
+                            >
+                              <LogOut className="text-red-700" />
+                              Logout
+                            </button>
+                          </div>
                         </div>
-                        <div className="p-2 flex items-center gap-2">
-                          <button
-                            onClick={logoutRedirect}
-                            className="w-full p-2 flex items-center gap-2  rounded-lg  text-sm text-red-700 hover:bg-red-100"
-                          >
-                            <LogOut className="text-red-700" />
-                            Logout
-                          </button>
-                        </div>
-                      </div>
+                      </>
                     )}
                   </div>
                 ) : (
@@ -197,18 +205,24 @@ const MainNav = () => {
           </nav>
 
           {/* Mobile Hamburger Menu */}
-          <div className="md:hidden">
+          <div className="md:hidden flex gap-4">
             <button
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              onClick={toggleSearch}
+              className="lg:hidden w-8 h-8 flex items-center justify-center rounded-full bg-gray-300 hover:text-red-500"
+            >
+              <Search size={18} />
+            </button>
+            <button
+              onClick={toggleProfile}
               className="text-gray-600 hover:text-red-500"
             >
-              {isMenuOpen ? (
+              {isProfileOpen ? (
                 <X size={28} />
               ) : (
                 <div className="relative">
                   <Menu size={28} />
                   {carts.length > 0 && (
-                    <span className="absolute -top-3 -right-1 rounded-full px-2 py-0.5 text-center text-xs bg-red-500 text-white">
+                    <span className="absolute -top-3 -right-1 rounded-full px-1.5 py-0.5 text-center text-xs bg-red-500 text-white">
                       {carts.length}
                     </span>
                   )}
@@ -219,22 +233,55 @@ const MainNav = () => {
         </div>
       </div>
 
+      {/* Search Popup for Mobile/Tablet */}
+      {isSearchOpen && (
+        <>
+          <div
+            className="fixed inset-0 bg-black bg-opacity-50 z-40"
+            onClick={toggleSearch}
+          />
+          <div className="fixed inset-x-0 top-0 bg-white p-4 shadow-lg z-50 animate-slide-down">
+            <div className="container mx-auto ">
+              <div className="flex justify-center items-center gap-4">
+                <SearchText 
+                setIsSearchOpen={setIsSearchOpen}
+                />
+                <button
+                  onClick={toggleSearch}
+                  className="p-2 hover:text-red-500"
+                >
+                  <X size={24} />
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+
+      {/* Overlay for Mobile Menu */}
+      {isProfileOpen && (
+        <div
+          className="fixed inset-0  bg-transparent z-20 md:hidden"
+          onClick={() => setIsProfileOpen(false)}
+        />
+      )}
+
       {/* Hamburger Menu Content */}
-      {isMenuOpen && (
+      {isProfileOpen && (
         <div className="absolute top-20 left-0 w-full bg-white shadow-lg z-20 md:hidden">
           <ul className="flex flex-col items-center gap-4 py-4">
-            {user &&
-            <li>
-              <span className=" text-red-500 transition hover:text-red-700 text-xl">
-                {user.name}
-              </span>
-            </li>
-            }
+            {user && (
+              <li>
+                <span className=" text-red-500 transition hover:text-red-700 text-xl">
+                  {user.name.substring(0, user.name.indexOf(" ")) || user.name}
+                </span>
+              </li>
+            )}
             <li>
               <NavLink
                 to={"/"}
                 end
-                onClick={() => setIsMenuOpen(false)}
+                onClick={() => setIsProfileOpen(false)}
                 className="text-gray-600 hover:text-red-500"
               >
                 Home
@@ -243,7 +290,7 @@ const MainNav = () => {
             <li>
               <NavLink
                 to={"/shop"}
-                onClick={() => setIsMenuOpen(false)}
+                onClick={() => setIsProfileOpen(false)}
                 className="text-gray-600 hover:text-red-500"
               >
                 Shop
@@ -252,13 +299,13 @@ const MainNav = () => {
             <li className="relative">
               <NavLink
                 to={"/cart"}
-                onClick={() => setIsMenuOpen(false)}
+                onClick={() => setIsProfileOpen(false)}
                 className="text-gray-600 hover:text-red-500"
               >
                 Cart
               </NavLink>
               {carts.length > 0 && (
-                <span className="absolute -top-3 -right-4 rounded-full px-2 text-xs bg-red-500 text-white">
+                <span className="absolute -top-3 -right-4 rounded-full px-1.5 py-0.5 text-xs bg-red-500 text-white">
                   {carts.length}
                 </span>
               )}
@@ -268,24 +315,23 @@ const MainNav = () => {
                 <li>
                   <NavLink
                     to={"/user/history"}
-                    onClick={() => setIsMenuOpen(false)}
+                    onClick={() => setIsProfileOpen(false)}
                     className="text-gray-600 hover:text-red-500"
                   >
                     History
                   </NavLink>
                 </li>
                 {user.role === "admin" && (
-                          <li>
-                            <NavLink
-                              to={"/admin"}
-                              onClick={() => setIsMenuOpen(false)}
-                              className="text-gray-600 hover:text-red-500"
-                            >
-
-                              Dashboard Admin
-                            </NavLink>
-                          </li>
-                        )}
+                  <li>
+                    <NavLink
+                      to={"/admin"}
+                      onClick={() => setIsProfileOpen(false)}
+                      className="text-gray-600 hover:text-red-500"
+                    >
+                      Dashboard Admin
+                    </NavLink>
+                  </li>
+                )}
 
                 <li>
                   <button
@@ -301,7 +347,7 @@ const MainNav = () => {
                 <li>
                   <NavLink
                     to={"/register"}
-                    onClick={() => setIsMenuOpen(false)}
+                    onClick={() => setIsProfileOpen(false)}
                     className="text-gray-600 hover:text-red-500"
                   >
                     Register
@@ -310,7 +356,7 @@ const MainNav = () => {
                 <li>
                   <NavLink
                     to={"/login"}
-                    onClick={() => setIsMenuOpen(false)}
+                    onClick={() => setIsProfileOpen(false)}
                     className="text-gray-600 hover:text-red-500"
                   >
                     Login
