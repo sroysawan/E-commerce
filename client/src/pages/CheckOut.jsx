afb0b23d4dd7091554ee18171b2ba1c27674e193
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-13 20:31:44 +0700

    refactor: Update CheckOut layout for improved spacing and responsiveness

--- client/src/pages/CheckOut.jsx
+++ client/src/pages/CheckOut.jsx
@@ -3,7 +3,7 @@ import SummaryCard from '../components/cart/SummaryCard'
 
 const CheckOut = () => {
   return (
-    <div className='pt-8 container mx-auto'>
+    <div className='container mx-auto px-0 sm:px-0 md:px-0 lg:px-2 xl:px-0 mt-4 xl:mt-8'>
       <SummaryCard />
     </div>
   )
